49c09be4b2016a8b92e37480a9f5cb90b4194701
Amrit Lahari <f20230442@pilani.bits-pilani.ac.in>
2025-04-14 02:11:27 +0530

    Add files via upload

---
+++ check_data.py
@@ -0,0 +1,15 @@
+import json
+
+with open('data.json') as f:
+    data = json.load(f)
+
+# Get the first station's data
+first_station = next(iter(data['data'].values()))
+print("First station keys:", first_station.keys())
+
+# Get the first project's data
+if 'project_details' in first_station:
+    first_project = next(iter(first_station['project_details'].values()))
+    print("\nFirst project keys:", first_project.keys())
+    print("\nFirst project mode:", first_project.get('mode'))
+    print("\nFirst project data:", json.dumps(first_project, indent=2)) 
